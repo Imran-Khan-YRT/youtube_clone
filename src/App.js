e2281154454bc81f3786b28ea59282710f26417e
ImranKhanYRT <imran.khan.yrt@gmail.com>
2023-12-27 17:04:45 +0600

    add component files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/system'
+
+
+import { Feed, ChannelDetails, SearchFeed, Navbar, VideoDetails } from "./components"
 
 const App = () => {
     return (
-        <div>App</div>
+        <BrowserRouter>
+            <Box sx={{ backgroundColor: "#000" }}>
+                <Navbar />
+                <Routes>
+                    <Route path='/' exact element={<Feed />} />
+                    <Route path='/video/:id' element={<VideoDetails />} />
+                    <Route path='/channel/:id' element={<ChannelDetails />} />
+                    <Route path='/search/:searchTerm' element={<SearchFeed />} />
+                </Routes>
+            </Box>
+        </BrowserRouter>
     )
 }
 
